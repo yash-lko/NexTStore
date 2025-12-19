9b820f95be682de76da9d8cd197e9c7ee6c03824
yashwant <yashwantbit@gmail.com>
2025-12-19 14:52:03 +0530

    Added Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { Search } from 'lucide-react'
+import { getProducts } from '@/lib/product';
 const SearchComp = () => {
+    const [search , setSearch]=useState('');
+    console.log(search, "search")
   return (
      <div className="hidden md:flex items-center flex-1 max-w-xl">
                     <div className="w-full relative">
                         <input
                             type="text"
+                            onChange={(e)=>setSearch(e.target.value)} 
+                            name='search'
                             placeholder="Search for products..."
                             className="w-full rounded-sm py-2 pl-4 pr-10 text-sm border border-gray text-black"
                         />
-                        <Search className="absolute right-3 top-2.5 text-pink-500 w-5 h-5" />
+                        <Search  className="absolute right-3 top-2.5 text-pink-500 w-5 h-5" />
                     </div>
                 </div>
   )
